4aea625d20851232ee4d4768dd56ea0ffd29a71e
Lucas William <lucaswilliameufrasio@gmail.com>
2020-08-07 06:52:41 -0300

    chore: add some props to make dynamic items

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,45 +1,81 @@
 import React from 'react'
 
+import PropTypes from 'prop-types'
+
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api'
 
-const TeacherItem: React.FC = () => {
+export interface ITeacher {
+  cost: Number
+  subject: string
+  user: {
+    id: Number
+    name: string
+    bio: string
+    avatar: string
+    whatsapp: string
+  }
+}
+
+interface ITeacherItemProps {
+  teacher: ITeacher
+}
+
+const TeacherItem: React.FC<ITeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.user.id
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars1.githubusercontent.com/u/34021576?v=4"
-          alt="Lucas William"
-        />
+        <img src={teacher.user.avatar} alt={teacher.user.name} />
 
         <div>
-          <strong>Lucas William</strong>
-          <span>Física</span>
+          <strong>{teacher.user.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta dos métodos computacionais da Física Computacional. (Quem
-        dera)
-        <br />
-        <br />
-        Adora desprezar a resistência do ar e esquecer de converter a unidade.
-      </p>
+      <p>{teacher.user.bio}</p>
 
       <footer>
         <p>
           Preço/Hora
-          <strong>R$ 400,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
 
-        <button type="button">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.user.whatsapp}`}
+          type="button"
+        >
           <img src={whatsappIcon} alt="Whatsapp icon" />
           Entrar em Contato
-        </button>
+        </a>
       </footer>
     </article>
   )
+}
+
+TeacherItem.propTypes = {
+  teacher: PropTypes.shape({
+    cost: PropTypes.number.isRequired,
+    subject: PropTypes.string.isRequired,
+    user: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      bio: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      whatsapp: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
 }
 
 export default TeacherItem

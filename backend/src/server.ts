295c4953c2c9427bdd6011af8fd026dcb765afcf
Lucas William <lucaswilliameufrasio@gmail.com>
2020-08-07 02:57:26 -0300

    chore: add dotenv config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,8 @@
+import * as dotenv from 'dotenv'
+import * as paths from 'path'
+
+dotenv.config({ path: paths.resolve(__dirname, '..', '.env') })
+
 import express from 'express'
 import env from './config/env'
 

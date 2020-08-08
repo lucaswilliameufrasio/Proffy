026907b9d8c11c48d4533fcc5b1dcffafbdf2600
Lucas William <lucaswilliameufrasio@gmail.com>
2020-08-08 18:18:30 -0300

    feat: add Landing screen

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import { View, Image, Text } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
+import landingImg from '../../assets/images/landing.png'
+import giveClassesIcon from '../../assets/images/icons/give-classes.png'
+import heartIcon from '../../assets/images/icons/heart.png'
+import studyIcon from '../../assets/images/icons/study.png'
+import api from '../../services/api'
+import styles from './styles'
+
+const Landing: React.FC = () => {
+  const [totalConnections, setTotalConnections] = useState(0)
+
+  const { navigate } = useNavigation()
+
+  useFocusEffect(() => {
+    async function fetchTotalConnections() {
+      const {
+        data: { total }
+      } = await api.get('connections')
+
+      setTotalConnections(total)
+    }
+
+    fetchTotalConnections()
+  })
+
+  function handleNavigateToGiveClassesPage() {}
+
+  function handleNavigateToStudyPages() {}
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.banner} source={landingImg} />
+
+      <Text style={styles.title}>
+        Seja bem-vindo, {'\n'}
+        <Text style={styles.titleBold}>O que deseja fazer?</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <RectButton
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}
+        >
+          <Image source={studyIcon} />
+
+          <Text style={styles.buttonText}>Estudar</Text>
+        </RectButton>
+
+        <RectButton
+          onPress={handleNavigateToGiveClassesPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
+          <Image source={giveClassesIcon} />
+
+          <Text style={styles.buttonText}>Dar aulas</Text>
+        </RectButton>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        Total de {totalConnections} conexões já realizadas{' '}
+        <Image source={heartIcon} />
+      </Text>
+    </View>
+  )
+}
+
+export default Landing
